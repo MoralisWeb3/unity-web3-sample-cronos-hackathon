9a0eeeb5cda51fb693e4e62ca5816a4ef30dabbd
dPradellMoralis <99496843+dpradell-moralis@users.noreply.github.com>
2022-08-10 09:32:23 +0200

    Added animations

---
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace NFT_PowerUp
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("Main Custom Components")]
+        public PlayerInputController input;
+        public PlayerMovement movement;
+    }
+}
+
